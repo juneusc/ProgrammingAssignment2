6e0cf2bb3523a8cb3a6d9d1e50aaf8c063851596
juneusc <liuzhejun630@gmail.com>
2016-08-27 13:48:50 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,18 @@
-## The below functions can cache the inverse of a matrix and retrieve the cached value instead of recomputing when needed.
+## The below functions ake advantage of the scoping rules of R. 
+## They can cache the inverse of a matrix and retrieve the 
+## cached value instead of recomputing when needed.
 
-## This function creates a matrix that caches its inverse.
+
+## This function below creates a matrix and returns a list that contains a function to
+## set and get value of the matrix, and
+## set and get the value of the inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
               n <- NULL
             set <- function (y) {
              x <<- y
              n <<- NULL
-}
+            }
             get <- function ()x
          setInv <- function (Inv) n <<- Inv
          getInv <- function ()n
@@ -15,7 +20,7 @@ makeCacheMatrix <- function(x = matrix()) {
                setInv = setInv, getInv = getInv)
 }
 
-## The following function calculates the inverse of the special "matrix" created with the above function. 
+## The following function calculates the inverse of the special "matrix" created by the above function. 
 ## However, it first checks to see if the inverse has already been calculated. 
 ## If so, it gets the inverse from the cache and skips the computation. 
 ## Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setInv function.
@@ -25,9 +30,9 @@ cacheSolve <- function(x, ...) {
             if(!is.null(n)) {
             message ("getting cached matrix")
             return (n)
-}
-          matrix <- x$get()
-               n <- solve(matrix, ...)
+            }
+          mat <- x$get()
+               n <- solve(mat, ...)
                x$setInv (n)
                n
 }
